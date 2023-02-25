8fa78fda888358799aa17a47858947fb3c424f7a
pwkpwk2022 <120775658+pwkpwk2022@users.noreply.github.com>
2023-02-26 05:23:33 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 # Device
-export FOX_BRANCH="fox_9.0"
-export DT_LINK="https://gitlab.com/OrangeFox/device/beryllium.git -b fox_9.0"
+export FOX_BRANCH="fox_11.0"
+export DT_LINK=https://github.com/OrangeFoxRecovery/device_qin_k61v1_64_bsp"
 
-export DEVICE="beryllium"
-export OEM="xiaomi"
+export DEVICE="k61v1_64_bsp"
+export OEM="qin"
 
 # Build Target
 ## "recoveryimage" - for A-Only Devices without using Vendor Boot
 ## "bootimage" - for A/B devices without recovery partition (and without vendor boot)
 ## "vendorbootimage" - for devices Using vendor boot for the recovery ramdisk (Usually for devices shipped with Android 12 or higher)
-export TARGET="recoveryimage"
+export TARGET="bootimage"
 
 export OUTPUT="OrangeFox*.zip"
 
