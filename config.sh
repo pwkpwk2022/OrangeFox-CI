2f8e2d6868e859c0c01941c7aacdecc1a5d866ab
pwkpwk2022 <120775658+pwkpwk2022@users.noreply.github.com>
2023-03-02 10:14:50 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -22,7 +22,7 @@ DEPS=(
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=pwk"
+export EXTRA_CMD="export OF_MAINTAINER=Pwk"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
