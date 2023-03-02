b5786b0719594067846d01985ecea61da484a7ea
pwkpwk2022 <120775658+pwkpwk2022@users.noreply.github.com>
2023-03-02 16:37:27 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -31,6 +31,6 @@ export OF_USE_LATEST_MAGISK=true
 # Not Recommended to Change
 export SYNC_PATH="$HOME/work" # Full (absolute) path.
 export USE_CCACHE=1
-export CCACHE_SIZE="50G"
+export CCACHE_SIZE="60G"
 export CCACHE_DIR="$HOME/work/.ccache"
 export J_VAL=16
