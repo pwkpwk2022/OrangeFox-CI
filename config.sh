7c88c4594e66f012d8a6eed41b2a4a9e16a05da2
pwkpwk2022 <120775658+pwkpwk2022@users.noreply.github.com>
2023-03-07 21:21:38 +0800

    Update config.sh

--- config.sh
+++ config.sh
@@ -22,11 +22,11 @@ DEPS=(
 )
 
 # Extra Command
-export EXTRA_CMD="export OF_MAINTAINER=Pwkbuilder"
+export EXTRA_CMD="export OF_MAINTAINER=Pwk"
 
 # Magisk
 ## Use the Latest Release of Magisk for the OrangeFox addon
-export OF_USE_LATEST_MAGISK=true
+export OF_USE_LATEST_MAGISK=false
 
 # Not Recommended to Change
 export SYNC_PATH="$HOME/work" # Full (absolute) path.
